6a1abd142efb27839706e8c2e6dc9528d3a78c63
Sebastian Ossinger <sebastian.ossinger@mail.de>
2018-05-14 18:47:24 +0200

    keep Bulk samples, because they don't seem to degrade data

--- quickcheck.py
+++ quickcheck.py
@@ -13,7 +13,7 @@ for file in glob.glob('res/*.csv'):
                  encoding="utf-16", parse_dates=[13, 14],
                  infer_datetime_format=True,
                  index_col=[4, 8, 13])
-    df = pd.concat([df, read[~read.index.get_level_values(0).str.contains('Bulk')]])
+    df = pd.concat([df, read])
 print('\nFinished Loading')
 print('Sorting')
 df = df.sort_index()
