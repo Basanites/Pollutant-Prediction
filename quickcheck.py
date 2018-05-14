082bfba639999264dc6bd127cede0b18eeec689b
Sebastian Ossinger <sebastian.ossinger@mail.de>
2018-05-14 18:42:08 +0200

    group by EoI code because of the naming convention of sampling points

--- quickcheck.py
+++ quickcheck.py
@@ -12,7 +12,7 @@ for file in glob.glob('res/*.csv'):
     read = pd.read_csv(file,
                  encoding="utf-16", parse_dates=[13, 14],
                  infer_datetime_format=True,
-                 index_col=[5, 8, 13])
+                 index_col=[4, 8, 13])
     df = pd.concat([df, read[~read.index.get_level_values(0).str.contains('Bulk')]])
 print('\nFinished Loading')
 print('Sorting')
