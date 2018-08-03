c9199c922e4fedc2276a05169ef8929613f5b091
Sebastian Ossinger <sebastian.ossinger@mail.de>
2018-08-03 14:51:56 +0200

    add docstrings

--- timeseries/predictions.py
+++ timeseries/predictions.py
@@ -25,7 +25,22 @@ class LinearRegressionPredictor(Predictor):
 
 
 class DecisionTreePredictor(Predictor):
+    """
+    Provides functionality to predict outputs by learning the training data using a decision tree.
+    """
+
     def __init__(self, traindata_x, traindata_y, testdata_x, depth, testdata_y=None, mode=None, steps=1):
+        """
+        Initializes the Decision Tree Predictor object
+
+        :param traindata_x: training x vector
+        :param traindata_y: training y vector
+        :param testdata_x:  testing x vector (the values to predict from)
+        :param depth:       the maximum depth to use for the tree
+        :param testdata_y:  testing y vector (only used when testing model accuracy)
+        :param mode:        one of 'multimodel' and 'recursive' for multistep forecast
+        :param steps:       only used when mode is set. Number of steps for multistep forecast
+        """
         super(DecisionTreePredictor, self).__init__(traindata_x, traindata_y, testdata_x, testdata_y)
         self.type = mode.lower()
         self.steps = steps
@@ -83,7 +98,7 @@ class DecisionTreePredictor(Predictor):
 
 class RandomForestPredictor(Predictor):
     """
-    Provides functionality to predict the output values by learning the training data
+    Provides functionality to predict the output values by learning the training data by using a random forest.
     """
 
     def __init__(self, traindata_x, traindata_y, testdata_x, n_estimators, testdata_y=None, mode=None, steps=1):
@@ -156,7 +171,7 @@ class RandomForestPredictor(Predictor):
 
 class KNearestNeighborsPredictor(Predictor):
     """
-    Provides functionality to predict values from given inputs after having learned from training data.
+    Provides functionality to predict values from given inputs after having learned from training data by using KNN.
     """
 
     def __init__(self, traindata_x, traindata_y, testdata_x, n_neighbors, weights, testdata_y=None, mode=None,
@@ -231,7 +246,23 @@ class KNearestNeighborsPredictor(Predictor):
 
 
 class ETSPredictor(Predictor):
-    def __init__(self, traindata_x, traindata_y, testdata_x, testdata_y, trendtype, seasontype, seasonlength: int):
+    """
+    Provides functionality to predict outputs by learning the training data using ETS
+    """
+
+    def __init__(self, traindata_x, traindata_y, testdata_x, seasonlength: int, trendtype='additive',
+                 seasontype='additive', testdata_y=None):
+        """
+        Initializes the ETS Predictor object
+
+        :param traindata_x:     training x vector
+        :param traindata_y:     training y vector
+        :param testdata_x:      testing x vector (the values to predict from)
+        :param trendtype:       type of ETS trend component. One of 'multiplicative' and 'additive'. Default 'additive'
+        :param seasontype:      type of ETS season component. One of 'multiplicative' and 'additive'. Default 'additive'
+        :param seasonlength:    the length to use for the season according to the given data
+        :param testdata_y:      testing y vector (only used when testing model accuracy)
+        """
         super(ETSPredictor, self).__init__(traindata_x, traindata_y, testdata_x, testdata_y)
         self.model = ExponentialSmoothing(self.train['y'],
                                           trend=trendtype,
@@ -239,26 +270,67 @@ class ETSPredictor(Predictor):
                                           seasonal_periods=seasonlength).fit(optimized=True)
 
     def predict(self, steps: int):
+        """
+        Predicts n future steps from the given training data
+
+        :param steps:   how many steps to predict
+        :return:        the values of the forecast steps using the same period as the training data
+        """
         self.y_ = self.model.forecast(steps)
         return self.y_
 
 
 class ARIMAPredictor(Predictor):
-    def __init__(self, traindata_x, traindata_y, testdata_x, testdata_y, order):
+    """
+    Provides functionality to predict from training data using ARIMA model
+    """
+
+    def __init__(self, traindata_x, traindata_y, testdata_x, order, testdata_y=None):
+        """
+        Initializes the ARIMA Predictor object
+
+        :param traindata_x: training x vector
+        :param traindata_y: training y vector
+        :param testdata_x:  testing x vector (the values to predict from)
+        :param order:       TODO triple? of the pdq values for ARIMA
+        :param testdata_y:  testing y vector (only used when testing model accuracy)
+        """
         super(ARIMAPredictor, self).__init__(traindata_x, traindata_y, testdata_x, testdata_y)
         self.model = ARIMA(traindata_x, order=order)
 
     def predict(self):
+        """
+        Predicts n future steps from the end of training data
+
+        :return: the values of the forecast steps
+        """
         self.model = self.model.fit(disp=0)
         pass
 
 
 class ProphetPredictor(Predictor):
-    def __init__(self, traindata_x, traindata_y, testdata_x, testdata_y):
+    """
+    Provides functionality to predict from training data using FBProphet
+    """
+
+    def __init__(self, traindata_x, traindata_y, testdata_x, testdata_y=None):
+        """
+        Initializes the Prophet Predictor object
+        :param traindata_x: training x vector
+        :param traindata_y: training y vector
+        :param testdata_x:  testing x vector (the values to predict from)
+        :param testdata_y:  testing y vector (only used when testing model accuracy)
+        """
         super(ProphetPredictor, self).__init__(traindata_x, traindata_y, testdata_x, testdata_y)
         self.model = Prophet().fit(pd.DataFrame(data={'ds': self.train['x'], 'y': self.train['y']}))
 
     def predict(self, steps: int):
+        """
+        Predicts n steps from the end of training data
+
+        :param steps:   the amount of steps to predict further
+        :return:        the predicted values
+        """
         future = self.model.make_future_dataframe(periods=steps, freq=self.train['x'].infer_freq(warn=False))
         self.y_ = self.model.predict(future)['yhat']
         return self.y_
@@ -270,6 +342,14 @@ class LSTMPredictor(Predictor):
 
 
 def create_artificial_features(series, frequency='H', steps=7):
+    """
+    Creates artificial features for a given series
+
+    :param series:      the base series to use
+    :param frequency:   the frequency of values in the series
+    :param steps:       the amount of steps to lag the series by
+    :return:            the dataframe containing the artificial features for the input series
+    """
     lagged = create_lagged_features(series, frequency, steps)
 
     statistics = lagged
@@ -291,6 +371,15 @@ def create_artificial_features(series, frequency='H', steps=7):
 
 
 def create_lagged_features(series, frequency='H', steps=7):
+    """
+    Creates a dataframe from a series containing the original series and the lagged values for the specified amount of
+    steps.
+
+    :param series:      the series to use
+    :param frequency:   the frequency of the values by which to shift
+    :param steps:       the amount of steps to shift
+    :return:            the shifted dataframe
+    """
     lagged = pd.DataFrame()
 
     for i in range(1, steps + 1):
