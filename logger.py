b93002ba9f3c7278559ca241b5e4b8c1710ad49b
Sebastian Ossinger <sebastian.ossinger@mail.de>
2018-08-18 19:41:17 +0200

    log console outputs to file

--- logger.py
+++ logger.py
@@ -1,8 +1,34 @@
-def log(string, level=0):
-    arrow = ''
-    for i in range(1, level + 1):
-        arrow += '--'
-    if len(arrow):
-        arrow += '>'
+from datetime import datetime
+import os
 
-    print(arrow, ' ', string)
+
+class Logger:
+
+    def __init__(self, log_location):
+        if not os.path.isfile(log_location):
+            open(log_location, 'x')
+        self.log_location = log_location
+
+    def log(self, string, level=0):
+        arrow = ''
+        for i in range(1, level + 1):
+            arrow += '--'
+        if len(arrow):
+            arrow += '>'
+            output = arrow + ' ' + string
+        else:
+            output = string
+
+        print(output)
+        self.write_to_logfile(output)
+
+    def append_file(self, string, location):
+        if not os.path.isfile(location):
+            open(location, 'x')
+
+        with open(location, 'a') as file:
+            file.write(string)
+
+    def write_to_logfile(self, string):
+        with open(self.log_location, 'a') as file:
+            file.write(f'{datetime.now()}: {string}\n')
